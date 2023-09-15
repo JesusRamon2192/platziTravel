5f8df7c527b5fb137a095e16282e0c207e196911
jesus <jesus.ramon2192@gmail.com>
2023-09-15 16:44:44 -0600

    Clase 18 Agregando animaciones al proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,11 @@ module.exports = {
           secondary: '#8FA206',
           terciary: '#61AEC9',
         },
+        ringColor:{
+          'primary': '#CC2D4A',
+          'secondary': '#8FA206',
+          'tertiary': '#61AEC9',
+        },
         fontFamily: {
           Montserrat: ["Montserrat", "sans-serif"]
         }
